1443fb1ad3b12e65b7b7048c94d54aff98830ba4
epdtry <epdtry@epdtry.net>
2023-01-31 01:48:32 +0000

    compile astcenc library within build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,30 @@
-fn main() {
+use std::fs;
+use std::io;
+use cc::Build;
+
+fn main() -> io::Result<()> {
+    let mut b = Build::new();
+    b.cpp(true);
+
+    b.include("deps/astc-encoder/Source");
+    for entry in fs::read_dir("deps/astc-encoder/Source")? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.extension() != Some("cpp".as_ref()) {
+            continue;
+        }
+        b.file(&path);
+    }
+
+    b.file("src/astcwrap.cpp");
+    b.define("ASTCENC_SSE", "0");
+    b.define("ASTCENC_AVX", "0");
+    b.define("ASTCENC_POPCNT", "0");
+    b.define("ASTCENC_VECALIGN", "16");
+    b.opt_level(3);
+    b.compile("astcwrap");
+
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rustc-link-lib=astcwrap");
-    println!("cargo:rustc-link-search=target/misc");
+
+    Ok(())
 }
